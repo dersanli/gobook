dbfc2bfff1ba9033c388f599cebb96f89462e1dc
dersanli <dersanli@gmail.com>
2021-12-30 18:32:43 +0300

    Added new content

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import "fmt"
 
-func add(x, y int) int { //shortened parameter type declaration
+var c, python, java bool // variable declaration, package or function level
+
+func add(x, y int) int { // shortened parameter type declaration
 	return x + y
 }
 
@@ -13,12 +15,17 @@ func swap(x, y string) (string, string) { // multiple return from a function
 func split(sum int) (x, y int) { // named return values (x, y)
 	x = sum * 4 / 9
 	y = sum - x
-	return // naked return
+	return // naked return (can harm readability in longer functions)
 }
 
 func main() {
+	var i, j int = 1, 2 // variable declaration with initializers (type (int) can be omitted)
+	fmt.Println(i, j, c, python, java)
+
 	fmt.Println(add(661651651, 6548498))
+
 	a, b := swap("swap", "us") // handling multiple return values
 	fmt.Println(a, b)
+
 	fmt.Println(split(256))
 }
